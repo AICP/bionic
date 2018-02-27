rINanDO/android_bionic

libc: Allow devices the use of invalid pthread via board flag.
  * Use the TARGET_ALLOWS_INVALID_PTHREAD := true
    to prevent softboots with the error
    "invalid pthread_t 0x.. passed to libc" due to legacy
     proprietary libraries.

Change-Id: Ie1876e7cc65d9502a986ee1eb400cb9a34cacf29

libc/bionic/pthread_internal.cpp
@@ -110,6 +110,9 @@ pthread_internal_t* __pthread_internal_find(pthread_t thread_id) {
 
   // Historically we'd return null, but
   if (bionic_get_application_target_sdk_version() >= __ANDROID_API_O__) {
+#ifdef ALLOWS_INVALID_PTHREAD
+    __libc_format_log(ANDROID_LOG_WARN, "libc", "invalid pthread_t %p passed to libc", thread);
+#else
     if (thread == nullptr) {
       // This seems to be a common mistake, and it's relatively harmless because
       // there will never be a valid thread at address 0, whereas other invalid
@@ -120,6 +123,7 @@ pthread_internal_t* __pthread_internal_find(pthread_t thread_id) {
     } else {
       __libc_fatal("invalid pthread_t %p passed to libc", thread);
     }
+#endif
   }
   return nullptr;
 }

AICP/bionic

     proprietary libraries that hardcode the api level.

libc/bionic/pthread_internal.cpp
@@ -115,6 +115,9 @@ pthread_internal_t* __pthread_internal_find(pthread_t thread_id) {
 
   // Historically we'd return null, but
   if (bionic_get_application_target_sdk_version() >= __ANDROID_API_O__) {
+#ifdef ALLOWS_INVALID_PTHREAD
+     async_safe_format_log(ANDROID_LOG_WARN, "libc", "invalid pthread_t %p passed to libc", thread);
+#else
     if (thread == nullptr) {
       // This seems to be a common mistake, and it's relatively harmless because
       // there will never be a valid thread at address 0, whereas other invalid
@@ -125,6 +128,7 @@ pthread_internal_t* __pthread_internal_find(pthread_t thread_id) {
     } else {
       async_safe_fatal("invalid pthread_t %p passed to libc", thread);
     }
+#endif
   }
   return nullptr;
 }